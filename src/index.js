2b0a1bb35b98de07d9793c249d0f4afeece710f1
周立 <li.zhou@huilianyi.com>
2018-09-23 10:40:31 +0800

    share4

--- src/index.js
+++ src/index.js
@@ -11,8 +11,10 @@ const app = new Koa();
 // });
 
 //使用路由中间件
-app.use(router.router.routes())
-    .use(routerForAllow.allowedMethods());
+app.use(router.router.routes());
+//可以用链式写法
+//当发生请求错的时候，添加友好status响应等
+app.use(routerForAllow.allowedMethods());
 
 //服务端口
 app.listen(8113);
