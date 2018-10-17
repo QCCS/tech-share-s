9ec28d7bcc502e926cd318d62015b252bdffe685
周立 <li.zhou@huilianyi.com>
2018-10-17 20:02:49 +0800

    添加备份模块

---
+++ backup-db.js
@@ -0,0 +1,18 @@
+const Backup = require('mysql-backup-db');
+var config = {
+    mysql: {
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'mac123',
+        database: 'tech_share_dev',
+        port: '3306',
+        backupPath:"./src/backup-db/"
+    }
+};
+mysqlConf = config.mysql;
+//30分支备份
+var gapTime = 1000*60*30;
+gapTime = 3000;
+//最近3份
+var count = 3;
+Backup.backupWithGaptime(mysqlConf,gapTime,count);
