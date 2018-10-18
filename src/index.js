974af7fc2e009173e7a74fa42e196113d1911d37
周立 <li.zhou@huilianyi.com>
2018-10-18 09:53:29 +0800

    update middleware

--- src/index.js
+++ src/index.js
@@ -1,95 +1,55 @@
 import Koa from 'koa';
 import Router from 'koa-router';
 import staticServer from 'koa-static';
-import koaJwt from 'koa-jwt';
 import bodyParser from 'koa-bodyparser';
-import koaSwagger from 'koa2-swagger-ui';
-import uploader from 'koa2-file-upload';
 
-import indexController from './controller/index';
-import config from './config/index';
 //拦截校验token，解密token
 import validateToken from './middleware/validateToken';
 import logNote from './middleware/logNote';
-import views from 'koa-views';
+import uploaderConfig from './middleware/uploaderConfig';
+import errorDel from './middleware/errorDel';
+import swaggerConfig from './middleware/swaggerConfig';
+import koaJwtConfig from './middleware/koaJwtConfig';
+import viewsConfig from './middleware/viewsConfig';
+
+import indexController from './controller/index';
+import config from './config/index';
 import router from './route/router';
 import postModuleRouter from './route/postModuleRouter';
-
+// 路由实例
 const routerForAllow = new Router();
+// 创建服务实例
 const app = new Koa();
-//使用babel编译之后，输出的是跟路径，/
+// 使用babel编译之后，输出的是跟路径，/
 console.log(__dirname);
-//默认的静态文件
-let staticPath = process.cwd() + "/dist/static";
-app.use(staticServer(staticPath));
-//上传的图片
-let staticImgPath = process.cwd() + "/publicImg";
-app.use(staticServer(staticImgPath));
-//排除某些接口,不校验
-app.use(koaJwt({secret: config.secret.sign}).unless({
-    path: [
-        /^\/favicon\.ico/,
-        /^\/api\/blog/,
-        /^\/api\/login/,
-        /^\/img/,//静态图片
-        /^\/api\/upload/,//上传文件
-        /^\/doc/,//文档忽略
-        /^\/swagger/,
-        /^\/api\/register/
-    ]
-}));
-
-let options = {
-    "url": '/api/upload',
-    "provider": "local",//存储位置类型
-    // "mimetypes": ['image/png','image/bmp'],
-    // 如果没有配置,将不进行类型检查 http://www.freeformatter.com/mime-types-list.html
-    "folder": "publicImg/images",//上传文件夹,后面 images 与 urlPath images 保持一致，这样可以直接把返回的 url 保持，
-    "storeDir": 'img',//存储文件夹
-    "urlPath": "images"//获取的时候url，可以存储，设置上传文件夹为静态路由，之后直接访问
-}
-
-app.use(uploader(options));
-
-app.use(
-    koaSwagger({
-        routePrefix: '/swagger', // host at /swagger instead of default /docs
-        swaggerOptions: {
-            url: '/doc.json', // example path to json
-            //json可以考虑模块导入，复用
-        },
-    }),
-);
-//token错误校验必须在 koaJwt 之后
+// 输出绝对路径
+console.log(process.cwd());
+// 默认的静态文件
+app.use(staticServer("." + __dirname + "dist/static"));
+// 上传的图片
+app.use(staticServer("." + __dirname + "publicImg"));
+// 排除某些接口,不校验
+app.use(koaJwtConfig());
+// 上传图片
+app.use(uploaderConfig());
+// swagger 文档
+app.use(swaggerConfig());
+// token错误校验必须在 koaJwt 之后
 app.use(validateToken());
-
-// Must be used before any router is used
-// 无模板引擎
-// app.use(views(process.cwd() + '/dist/views'));
-// ejs模板引擎
-// 配置扩展名 ejs
-app.use(views(process.cwd() + '/dist/views', {
-    extension: 'ejs'
-    // map: {html: 'ejs'}
-}))
-
-//日志处理
+// 模板引擎配置
+app.use(viewsConfig());
+// 日志处理
 app.use(logNote());
-
-//请求体处理
+// 请求体处理
 app.use(bodyParser());
-//统一错误处理
-app.on('error', function (err, ctx) {
-    console.log(err);
-    console.log(ctx);
-});
-
+// 统一错误处理
+app.on('error', errorDel());
 // 使用路由中间件
 app.use(indexController)
     .use(router.router.routes())
     .use(postModuleRouter.router.routes())
     .use(routerForAllow.allowedMethods());
 
-//服务端口
+//监听端口
 app.listen(config.port);
-console.log("监听端口："+config.port)
+console.log("监听端口：" + config.port);
